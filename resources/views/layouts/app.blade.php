d5d406ccfbf54aeba5f38d859314d277cba1f759
AlessioConsolo <alessioconsolo3@gmail.com>
2024-09-28 19:18:27 +0200

    [UPD] added menu to header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Laravel Comics')</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    @vite(['resources/scss/app.scss', 'resources/js/app.js'])
 </head>
 <body>
     @include('partials.header')
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,8 +1,39 @@
-<header>
-    <nav>
-        <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/comics">Comics</a></li>
-        </ul>
-    </nav>
-</header>
+<div class="container">
+    <header>
+        <nav class="d-flex justify-content-between py-2">
+        <img src="{{ asset('images/dc-logo.png') }}" alt="Logo">
+            <ul class="nav align-items-center">
+                <li class="nav-item">
+                    <a class="nav-link active" href="/comics">CHARACTERS</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/comics">COMICS</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/comics">MOVIES</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/comics">TV</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/comics">GAMES</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/comics">COLLECTIBLES</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/comics">VIDEOS</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/comics">FANS</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/comics">NEWS</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/comics">SHOP</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</div>
