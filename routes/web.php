abd517eeabccd2b856803c210484727e60cab2eb
AlessioConsolo <alessioconsolo3@gmail.com>
2024-09-28 18:52:13 +0200

    [UPD] Added ComicController

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,10 @@
 <?php
 
+use App\Http\Controllers\ComicController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/comics', function () {
-    $comics = config('comics');
-    return view('comics.index', ['comics' => $comics]); // La vista con i fumetti
-});
+Route::get('/comics', [ComicController::class, 'index']);
